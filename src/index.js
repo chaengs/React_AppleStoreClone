db49b04f2a9355f29ac4b192a059456cb08c3b0a
chaeng <cathyshim222@naver.com>
2022-02-13 17:50:27 +0900

    App.js 완성

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import clayful from "clayful/client-js";
+import axios from "axios";
+
+clayful.config({
+  client: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjM5OGE4ZGZlMGVlOWJkYWMyNTkzZWY0ZGRkZGRmNmY0YzZhY2Y4MDkzNGE2OGFjNWI4NDA4OWQ0ZTA3NDYxMzkiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjQ0Mzk1Mjg3LCJzdG9yZSI6IktNVlE2NkVOMlBONy5MWTdRTVJQTlFBRlAiLCJzdWIiOiJKRVVaQTZMRTk1SlAifQ.I0ABA525d0CQrNAHJLe-YdJW9w9rkcCxf16UtHfmZoo'
+});
+
+clayful.install('request', require('clayful/plugins/request-axios')(axios));
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
